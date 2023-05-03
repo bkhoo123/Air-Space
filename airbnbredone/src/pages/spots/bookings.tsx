b5cc0e18004d20a06677912db1387decb7d94184
bkhoo123 <102838003+bkhoo123@users.noreply.github.com>
2023-05-02 21:32:10 -0700

    Favicon

--- airbnbredone/src/pages/spots/bookings.tsx
+++ airbnbredone/src/pages/spots/bookings.tsx
@@ -1,12 +1,15 @@
 import React, {useState, useEffect} from 'react'
 import { useUser } from '<prefix>/context/UserContext'
 import axios from 'axios'
+import { useRouter } from 'next/router'
 
 export default function Bookings() {
+    const router = useRouter()
     const {currentUser, setCurrentUser} = useUser()
     const [bookings, setBookings] = useState([])
     const [spots, setSpots] = useState([])
     const [toggle, setToggle] = useState(false)
+
 
     useEffect(() => {
 
@@ -23,7 +26,7 @@ export default function Bookings() {
     }, [])
 
     let filter = spots.filter((spot) => {
-        return spot.userId === currentUser?.id
+        return spot.userId === currentUser?.id 
     })
 
     
@@ -40,10 +43,10 @@ export default function Bookings() {
             {filter.map(spot => (
             <div className="flex flex-col gap-5 ">
               <div className="text-lg font-bold">{spot.title}</div>
-              <img className="h-72 w-96 rounded-md hover:scale-105 hover:cursor-pointer transition duration-300 ease-in" src={spot.spotImages[0]} alt="No Image" />
+              <img onClick={() => router.push(`/spots/${spot._id}`)}  className="h-72 w-96 rounded-md hover:scale-105 hover:cursor-pointer transition duration-300 ease-in" src={spot.spotImages[0]} alt="No Image" />
               <span>{spot.location}</span>
               <div className="flex gap-5">
-                <button className="border-2 border-violet-400 p-2 rounded-md hover:bg-purple-100 cursor-not-allowed">Bookings</button>
+                <button onClick={() => alert("Add your bookings on the spot details page")} className="border-2 border-violet-400 p-2 rounded-md hover:bg-purple-100 cursor-pointer">Bookings</button>
               </div>
               <div className="absolute h-auto w-4/6 top-30 left-30 z-50 bg-teal-100">
 
